d0ede48a02e6137548b1b909cf228c59f3cb410d
yuriy-m-dev <fath66426@gmail.com>
2020-02-21 19:40:15 +0200

    added search section

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.search__date input').datepicker({
+        inline: false,
+        position: "bottom right"
+    });
+
+    $('select, input[type="checkbox"]').styler();
+});
--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,9 @@ gulp.task('style', function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css',
+        'node_modules/air-datepicker/dist/css/datepicker.css',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.css'
     ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
@@ -32,7 +34,9 @@ gulp.task('style', function(){
 gulp.task('script', function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+        'node_modules/air-datepicker/dist/js/datepicker.js',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.js'
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
